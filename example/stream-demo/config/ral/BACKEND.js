b46e72dcf9da8d3c6fbd1f2939a907cbbc6ea1f8
hefangshi <hefangshi@gmail.com>
2015-05-08 00:05:54 +0800

    [example] add stream-demo

---
+++ example/stream-demo/config/ral/BACKEND.js
@@ -0,0 +1,35 @@
+module.exports.PROXY = {
+    protocol: 'http',
+    pack: 'stream',
+    unpack: 'string',
+    method: 'POST',
+    balance: 'roundrobin',
+    timeout: 5000,
+    retry: 0,
+    encoding: 'gbk', // wenku.baidu.com编码为GBK
+    path: '/',
+    server: [
+        {
+            host: 'wenku.baidu.com',
+            port: 80
+        }
+    ]
+};
+
+module.exports.DEMO_SERVER = {
+    protocol: 'http',
+    pack: 'form',
+    unpack: 'string',
+    method: 'POST',
+    balance: 'roundrobin',
+    encoding: 'utf-8', // 由于DEMO SERVER的返回值是经过转码的，因此直接使用UTF-8
+    timeout: 5000,
+    retry: 0,
+    path: '/',
+    server: [
+        {
+            host: '127.0.0.1',
+            port: 9032
+        }
+    ]
+};
---
+++ example/stream-demo/index.js
@@ -0,0 +1,56 @@
+var ral = require('./ral.js');
+var assert = require('assert');
+
+
+/** 
+stream 数据类型最常见的用途就是转发请求
+
+这种方式的优点是转发数据可以节省前端数据的解包和打包时间，并且无需等待前端传回的数据接收完毕就可以发起转发请求
+
+这种方式缺点是由于Node后端对传递的数据没有解包操作，所以无法知道具体传递的内容，也无法对传递的内容做修改
+
+示例中，会像DEMO_SERVER发起一个search请求，DEMO_SERVER在接收到请求后，转发至wenku.baidu.com，并将接收的结果直接返回
+**/
+
+var http = require('http');
+var server = http.createServer(function(req, res){
+    delete req.headers.host; // 删除host，避免后端识别host错误，绝大部分情况都建议这样处理
+    /* 如果使用express，可以传递
+        headers: req.headers, // 传递headers
+        path: req.path, // 传递path
+        query: req.query, // 传递path
+        method: req.method // 传递method
+    */
+    ral('PROXY', {
+        data: req,  // 直接将req作为stream源传递，主要是对body数据做流式转发
+        headers: req.headers, // 传递headers
+        path: req.url, // 传递path
+        method: req.method // 传递method
+    }).on('data', function (data) {
+        assert.notEqual(data.indexOf('Node.js'), -1);
+        res.write('<!-- from proxy -->');
+        res.write(data);
+        res.end();
+    }).on('error', function (err) {
+        assert.fail(err, null);
+    });
+}).listen(9032);
+
+
+// 请求发往 http://127.0.0.1:9032
+ral('DEMO_SERVER', {
+    data: {
+        word: 'Node.js'
+    },
+    path: '/search',
+    query: 'st=3'
+}).on('data', function (data) {
+    server.close();
+    // 接收到服务器返回的信息
+    // 检查proxy的信息
+    assert.notEqual(data.indexOf('from proxy'), -1);
+    assert.notEqual(data.indexOf('Node.js'), -1);
+}).on('error', function (err) {
+    server.close();
+    assert.fail(err, null);
+});
