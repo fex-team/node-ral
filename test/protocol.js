cddc9e4d67c61338dbdde231c5641a67e01fde3a
hefangshi <hefangshi@gmail.com>
2015-11-30 19:38:19 +0800

    req: fix crash when req aborted before resp

--- test/protocol.js
+++ test/protocol.js
@@ -310,7 +310,7 @@ describe('http protocol context', function () {
 });
 
 describe('soap protocol', function () {
-    it.skip('should request wsdl service successfully', function (done) {
+    it('should request wsdl service successfully', function (done) {
         var soapTest = require('./protocol/soap_protocol.js');
         var context = SoapProtocol.normalizeConfig(soapTest);
         context.method = 'GetCityForecastByZIP';
@@ -327,7 +327,7 @@ describe('soap protocol', function () {
         });
     });
 
-    it.skip('should request wsdl service with service.port successfully', function (done) {
+    it('should request wsdl service with service.port successfully', function (done) {
         var soapTest = require('./protocol/soap_protocol.js');
         var context = SoapProtocol.normalizeConfig(soapTest);
         context.soapService = 'Weather';
